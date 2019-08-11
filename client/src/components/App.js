7bb0993b5438b5a4929da9071e664b8ed9447b7c
Kellyanne Revenaugh <kellyrevenaugh@gmail.com>
2019-08-11 15:51:54 -0400

    set up database and server connections

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return <div>hello</div>;
+  return <div>hello!!</div>;
 };
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,47 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+var con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'cow'
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log('Connected!');
+});
 
 const app = express();
+
+//when someone makes get or post request to url
+
+app.get('/allcows', function(req, res) {
+  con.query('SELECT * FROM cows', function(err, data) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json({ cows: data });
+    }
+  });
+});
+
+app.post('/postcow', function(req, res) {
+  con.query(
+    `INSERT INTO cows (name, description) VALUES ("{req.body.name}","{req.body.description}")`,
+    function(err, data) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send('cow posted!');
+      }
+    }
+  );
+});
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
